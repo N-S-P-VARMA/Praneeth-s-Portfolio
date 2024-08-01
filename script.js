ded49839083f5340a5addd92471cc3689001a0ed
N-S-P-VARMA <113942545+N-S-P-VARMA@users.noreply.github.com>
2024-08-01 15:55:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let type = new Typed('#changing-text', {
+    strings : [ 'Front-end Developer', 'MS Office', 'React.js & Node.js','....' ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+
+})
